44ba5b2177ecb3ec43144be7468037372fffc7bc
KathleenDollard <kdollard@microsoft.com>
2023-06-07 06:19:06 -0700

    Added DatOutputUnit for Json, etc output.

---
+++ src/System.CommandLine.CliOutput/CliDataOutputUnit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.CommandLine.CliOutput
+{
+    public class CliDataOutputUnit : CliOutputUnit
+    {
+        public CliDataOutputUnit(IEnumerable<object> value, int indentLevel = 0)
+        {
+            Value = value;
+            IndentLevel = indentLevel;
+        }
+
+        public IEnumerable<object> Value { get; }
+    }
+}
