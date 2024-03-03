ca2953d906d2dc3c28193fe315f2b27cee51f203
KathleenDollard <kdollard@microsoft.com>
2024-03-03 08:16:57 -0500

    Added subsystem implementation

---
+++ src/System.CommandLine.Subsystems/CompletionsSubsystem.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace System.CommandLine.Subsystem;
+
+public class CompletionsSubsystem : CliSubsystem
+{
+    public CompletionsSubsystem(IAnnotationProvider? annotationProvider = null)
+        : base("Completions", annotationProvider, SubsystemKind.Completions)
+    { }
+
+    // TODO: Figure out trigger for completions
+    protected internal override bool GetIsActivated(ParseResult? parseResult)
+        => parseResult is null
+            ? false
+            : false;
+
+    protected internal override CliExit Execute(PipelineContext pipelineContext)
+    {
+        pipelineContext.ConsoleHack.WriteLine("Not yet implemented");
+        return CliExit.SuccessfullyHandled(pipelineContext.ParseResult);
+    }
+}
