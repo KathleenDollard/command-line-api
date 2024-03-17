74222316a997bc1010dab0608728c1c170f847b3
KathleenDollard <kdollard@microsoft.com>
2024-03-17 15:16:57 -0400

    Switched to OuterLocation from concatenated SourceName, also added simple test

--- src/System.CommandLine/Parsing/Location.cs
+++ src/System.CommandLine/Parsing/Location.cs
@@ -3,6 +3,7 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace System.CommandLine.Parsing
 {
@@ -11,47 +12,40 @@ namespace System.CommandLine.Parsing
         public const string Implicit = "Implicit";
         public const string Internal = "Internal";
         public const string User = "User";
+        public const string Response = "Response";
 
-        internal static Location CreateImplicit(int length)
-           => new(Implicit, -1, length);
-        internal static Location CreateInternal(int length)
-           => new(Internal, -1, length);
+        internal static Location CreateImplicit(int length, Location? outerLocation = null, int offset = 0)
+           => new(Implicit, -1, length, offset, outerLocation);
+        internal static Location CreateInternal(int length, Location? outerLocation = null, int offset = 0)
+           => new(Internal, -1, length, offset, outerLocation);
+        internal static Location CreateUser(int start, int length, Location? outerLocation = null, int offset = 0) 
+            => new Location(User, start, length, offset);
+        internal static Location CreateResponse(string responseSourceName, int start, int length, Location? outerLocation = null, int offset = 0)
+            => new Location($"{Response}:{responseSourceName}", start, length, offset);
 
-        public Location(string source, int start, int length, int offset)
+        
+        internal static Location FromOuterLocation(int start, int length, Location outerLocation, int offset = 0) 
+            => new(outerLocation.Source, start, length, offset, outerLocation);
+
+        public Location(string source, int start, int length, int offset = 0, Location? outerLocation = null)
         {
             Source = source;
             Start = start;
+            Length = length;
             Offset = offset;
-            Length = length;
+            OuterLocation = outerLocation;
         }
-
-        public Location(string source, int start, int length)
-        {
-            Source = source;
-            Start = start;
-            Length = length;
-            Offset = 0;
-        }
-
-        public static Location[] CreateList(string source, IReadOnlyList<string> args)
-        {
-            var ret = new Location[args.Count];
-            for (int i = 0; i < args.Count; i++)
-            {
-                ret[i] = new Location(source, i, args[i].Length, 0);
-            }
-            return ret;
-        }
-
-        internal static Location FromOuterLocation(Location location, int argPosition, int length, int offset = 0) 
-            => new(location.Source, argPosition, length, offset);
 
         public string Source { get; }
-        public int Start {  get; }
-        public int Offset {  get; }
+        public int Start { get; }
+        public int Offset { get; }
         public int Length { get; }
+        public Location? OuterLocation { get; }
 
         public bool IsImplicit
             => Source == Implicit;
+
+        public override string ToString() 
+            => $"{(OuterLocation is null ? "" : OuterLocation.ToString() + "; ")}{Source} [{Start}, {Length}, {Offset}]";
     }
 }
