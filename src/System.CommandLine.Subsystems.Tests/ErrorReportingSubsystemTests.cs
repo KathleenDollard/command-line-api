1ee8c9f3a23a2930de509bab522925f246102780
Jean Joeris <jeanjoeris@gmail.com>
2024-03-23 14:22:35 -0400

    Add unit tests for ErrorReportingSubsystem, with scope changes

--- src/System.CommandLine.Subsystems.Tests/ErrorReportingSubsystemTests.cs
+++ src/System.CommandLine.Subsystems.Tests/ErrorReportingSubsystemTests.cs
@@ -1,69 +1,71 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Reflection;
 using FluentAssertions;
 using Xunit;
 using System.CommandLine.Parsing;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace System.CommandLine.Subsystems.Tests;
 
-public class ErrorReportingSubsystemTests
-{
-    [Fact]
-    public void Report_when_single_error_writes_to_console_hack()
+    public class ErrorReportingSubsystemTests
     {
-        var error = new ParseError("a sweet error message");
-        var errors = new List<ParseError> { error };
-        var errorSubsystem = new ErrorReportingSubsystem();
-        var consoleHack = new ConsoleHack().RedirectToBuffer(true);
+        [Fact]
+        public void Report_when_single_error_writes_to_console_hack()
+        {
+            var error = new ParseError("a sweet error message");
+            var errors = new List<ParseError> { error };
+            var errorSubsystem = new ErrorReportingSubsystem();
+            var consoleHack = new ConsoleHack().RedirectToBuffer(true);
 
-        errorSubsystem.Report(consoleHack, errors);
+            errorSubsystem.Report(consoleHack, errors);
 
-        consoleHack.GetBuffer().Trim().Should().Be(error.Message);
-    }
+            consoleHack.GetBuffer().Trim().Should().Be(error.Message);
+        }
 
-    [Fact]
-    public void Report_when_multiple_error_writes_to_console_hack()
-    {
-        var error = new ParseError("a sweet error message");
-        var anotherError = new ParseError("another sweet error message");
-        var errors = new List<ParseError> { error, anotherError };
-        var errorSubsystem = new ErrorReportingSubsystem();
-        var consoleHack = new ConsoleHack().RedirectToBuffer(true);
+        [Fact]
+        public void Report_when_multiple_error_writes_to_console_hack()
+        {
+            var error = new ParseError("a sweet error message");
+            var anotherError = new ParseError("another sweet error message");
+            var errors = new List<ParseError> { error, anotherError };
+            var errorSubsystem = new ErrorReportingSubsystem();
+            var consoleHack = new ConsoleHack().RedirectToBuffer(true);
 
-        errorSubsystem.Report(consoleHack, errors);
+            errorSubsystem.Report(consoleHack, errors);
 
         consoleHack.GetBuffer().Trim().Should().Be($"{error.Message}{Environment.NewLine}{anotherError.Message}");
-    }
+        }
 
-    [Fact]
-    public void Report_when_no_errors_writes_nothing_to_console_hack()
-    {
-        var errors = new List<ParseError> { };
-        var errorSubsystem = new ErrorReportingSubsystem();
-        var consoleHack = new ConsoleHack().RedirectToBuffer(true);
+        [Fact]
+        public void Report_when_no_errors_writes_nothing_to_console_hack()
+        {
+            var errors = new List<ParseError> { };
+            var errorSubsystem = new ErrorReportingSubsystem();
+            var consoleHack = new ConsoleHack().RedirectToBuffer(true);
 
-        errorSubsystem.Report(consoleHack, errors);
+            errorSubsystem.Report(consoleHack, errors);
 
-        consoleHack.GetBuffer().Trim().Should().Be("");
-    }
+            consoleHack.GetBuffer().Trim().Should().Be("");
+        }
 
-    [Theory]
+        [Theory]
     [InlineData("-x")]
     [InlineData("-non_existant_option")]
     public void GetIsActivated_GivenInvalidInput_SubsystemIsActive(string input)
-    {
-        var rootCommand = new CliRootCommand {new CliOption<bool>("-v")};
-        var configuration = new CliConfiguration(rootCommand);
-        var errorSubsystem = new ErrorReportingSubsystem();
+        {
+            var rootCommand = new CliRootCommand {new CliOption<bool>("-v")};
+            var configuration = new CliConfiguration(rootCommand);
+            var errorSubsystem = new ErrorReportingSubsystem();
         IReadOnlyList<string> args = [""];
         Subsystem.Initialize(errorSubsystem, configuration, args);
 
-        var parseResult = CliParser.Parse(rootCommand, input, configuration);
-        var isActive = Subsystem.GetIsActivated(errorSubsystem, parseResult);
+            var parseResult = CliParser.Parse(rootCommand, input, configuration);
+            var isActive = Subsystem.GetIsActivated(errorSubsystem, parseResult);
 
         isActive.Should().BeTrue();
-    }
+        }
 
     [Theory]
     [InlineData("-v")]
